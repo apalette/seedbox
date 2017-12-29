86e1b52e275f1e0e1d3e285ba6034b69c0e60dff
apalette <aurelie@codeuz.com>
2017-12-29 16:35:02 +0100

    Create User + Login

---
+++ code/resources/views/layouts/auth.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    
+    <!-- Favicon -->
+    <link type="image/x-icon" rel="icon" href="{{ asset('favicon.ico') }}">
+    <link type="image/x-icon" rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+
+    <!-- Styles -->
+    <link href="{{ asset('app/css/main.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-default navbar-static-top navbar-inverse">
+            <div class="container">
+                <div class="navbar-header">
+
+                    <!-- Branding Image -->
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
+                </div>
+            </div>
+        </nav>
+
+        @yield('content')
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('app/js/main.js') }}"></script>
+</body>
+</html>
