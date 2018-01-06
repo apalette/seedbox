b521f8b75aabea75e30a6e5c82906bbf4c0fc70a
apalette <aurelie@codeuz.com>
2018-01-06 19:20:29 +0100

    Change Download Episode Name for OSMC

--- code/app/Http/Controllers/SeedboxController.php
+++ code/app/Http/Controllers/SeedboxController.php
@@ -120,9 +120,10 @@ class SeedboxController extends Controller
 		}
 		
 		// Save download
+		$season_path = 'season'.str_pad($season->number, 2, '0', STR_PAD_LEFT);
 		$download = new Download;
 		$download->url = $request->download_url;
-		$download->destination = 'series/'.$tvshow->tag.'/season'.str_pad($season->number, 2, '0', STR_PAD_LEFT).'/'.str_pad($request->episode_number, 2, '0', STR_PAD_LEFT);
+		$download->destination = 'series/'.$tvshow->tag.'/'.$season_path.'/'.$tvshow->tag.'-'.$season_path.'-'.str_pad($request->episode_number, 2, '0', STR_PAD_LEFT);
 		$download->user_id = Auth::user()->id;
 		$download->save();
 		
